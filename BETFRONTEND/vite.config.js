50355a0c1800db2d3d201b786a3fee48bb2ea7ed
Androrun <106136947+Androrun@users.noreply.github.com>
2024-08-04 17:18:58 -0500

    Update vite.config.js

--- BETFRONTEND/vite.config.js
+++ BETFRONTEND/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: 'BETFRONTEND', // Raíz del proyecto para Vite
+  root: '.', // Raíz del proyecto para Vite es BETFRONTEND
   build: {
     outDir: '../dist', // Salida en la carpeta dist en el nivel superior
   },
