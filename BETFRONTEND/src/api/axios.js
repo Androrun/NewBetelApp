70f04eb8a828f7048d07291c4ac34026dac11048
Androrun <106136947+Androrun@users.noreply.github.com>
2024-08-04 15:12:04 -0500

    Update axios.js

--- BETFRONTEND/src/api/axios.js
+++ BETFRONTEND/src/api/axios.js
@@ -2,8 +2,9 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
+  baseURL: import.meta.env.VITE_API_URL, // Sin duplicar '/api'
   withCredentials: true,
 });
 
 export default client;
+
